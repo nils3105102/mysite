3cef82407a2f251849c07562366f81977d0a3f7f
nils3105102 <daniil.pron4enkov@gmail.com>
2019-08-06 18:50:23 +0300

    Simple Web Site

---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,8 @@
+<div class="jumbotron text-center my-sm-3">
+    <div class="container">
+        <h1>Welcome to my Site</h1>
+        <p class="lead">Welcome to our website. Nice to meet you.</p>
+
+    </div>
+
+</div>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', 'PagesController@getHome');
+Route::get('/about', 'PagesController@getAbout');
+Route::get('/contact', 'PagesController@getContact');
+/*Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});*/
+Route::post('/contact/submit', 'MessagesController@submit');
+Route::get('/messages', 'MessagesController@getMessages');
